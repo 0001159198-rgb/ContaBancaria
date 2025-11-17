6c7d5f2b9614f520016b030ef84d8ab4c89ce8b8
Davi Souza Carmo <0001159198@senaimgaluno.com.br>
2025-11-17 10:30:41 -0300

    Novo

---
+++ src/contabancaria.java
@@ -0,0 +1,54 @@
+public class contabancaria {
+    private String titular;
+    private int numeroConta;
+    private double saldo;
+
+    private contabancaria(String titular, int numeroConta, double saldo){
+        this.titular = titular;
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+    }
+
+    //Get e Set
+
+    public String getTitular(){
+        return this.titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public int getNumeroConta(){
+        return this.numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    // metodo de depositar
+    public void depositar(double valor){
+        // Se valor for maior que zero, o saldo vai ser depositado
+        if(valor > 0){
+            this.saldo += valor;
+            System.out.println("Saldo depositado");
+        }else{
+            System.out.println("Falha em depositar");
+        }
+    }
+    //Metodo de sacar o dinheiro
+    public void sacar(double valor){
+        if(valor > 0 && valor <= saldo){
+            this.saldo -= valor;
+        }
+    }
+}
+
